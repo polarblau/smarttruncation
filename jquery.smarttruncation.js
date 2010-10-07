ccecc695c2ae2d4e5ea1a415c20337e417de9a6c
Polarblau <polarblau@gmail.com>
2010-10-07 18:41:29 +0300

    Added caching of sizes.

--- jquery.smarttruncation.js
+++ jquery.smarttruncation.js
@@ -18,32 +18,47 @@
 		return $(this).each(function(i, e){
 			var $e = $(e);
 			
-			// let's get the width of the most common characters
-			// in the current font-size 
-			// TODO: cache results for same font settings!
-			var letters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ");
-			var numbers = "1 2 3 4 5 6 7 8 9 0".split(" ");
-			var other = "! § $ % & / ( ) = ? @ * ' + # - ; , : . < >".split(" ");
-			var all = letters
-		            .concat([" ", '"', "'"])
-			          .concat(numbers)
-			          .concat(other)
-			          .concat($.map(letters, function(letter){ return letter.toUpperCase(); }));
-			var sizes = {};
-			var $testWrapper = $('<span/>').css({
-				'visibility' : 'hidden', 
+			// cache 
+			if (!$(window).data('smarttruncation.sizecache')) 
+			  $(window).data('smarttruncation.sizecache', {});
+			  
+			var fontAttributes = {
 				'fontSize'   : $e.css('fontSize'),
 				'fontFamily' : $e.css('fontFamily'),
 				'fontWeight' : $e.css('fontWeight'),
 				'fontStyle'  : $e.css('fontStyle')
-			}).appendTo('body');
-			$.each(all, function(i, e){ sizes[e] = $testWrapper.text(e).width(); });
-			$testWrapper.remove();
+  		};
+  		
+  		// use font properties for cachekey
+      var cacheKey = (function(s){var r="";for(k in s)r+=s[k];return r;})(fontAttributes);
+      
+      // used cached sizes if available
+      var sizes = {};
+      if ($(window).data('smarttruncation.sizecache')[cacheKey]) {
+        sizes = $(window).data('smarttruncation.sizecache')[cacheKey];        
+      } else {
+        // let's get the width of the most common characters
+  			// in the current font-size
+  			var letters = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ");
+  			var numbers = "1 2 3 4 5 6 7 8 9 0".split(" ");
+  			var other = "! § $ % & / ( ) = ? @ * ' + # - ; , : . < >".split(" ");
+  			var all = letters
+  		            .concat([" ", '"', "'"])
+  			          .concat(numbers)
+  			          .concat(other)
+  			          .concat($.map(letters, function(letter){ return letter.toUpperCase(); }));
+			
+  			var $testWrapper = $('<span/>').css($.merge(fontAttributes, { 'visibility' : 'hidden' })).appendTo('body');
+  			$.each(all, function(i, e){ sizes[e] = $testWrapper.text(e).width(); });
+  			$testWrapper.remove();
+  			$(window).data('smarttruncation.sizecache')[cacheKey] = sizes;
+			}
 			
 			// wrap content in a inline element to get exact width
-			var $wrapper = $e.wrapInner('<span/>').find('span').css({
+			$e.data('wrapper', $e.wrapInner('<span/>').find('span').css({
 				'whiteSpace' : 'nowrap'
-			});
+			}));
+			var $wrapper = $e.data('wrapper');
 			var origText = $.trim($wrapper.text());
 			var outerWidth = $wrapper.width();
 			var tracking = parseInt($e.css('letterSpacing'), 10) || -1;
